0843f3893bb8685513e271c888e497e64c21187e
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-06-20 12:16:45 +0530

    Time: 20 ms (5.10%) | Memory: 50.2 MB (5.06%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string temp="";
+        string ans="";
+        int start=0;
+        unordered_map<int,string>m;
+        int i=1;
+        while(start<s.size()){
+            if(s[start]==' '){
+                if(temp!="") {
+                    m[i]=temp;
+                    temp="";
+                    i++;
+                }
+                while(start < s.size() && s[start]==' ') start++;
+                 if(start == s.size()) break;
+            }
+             temp=temp+s[start];
+             start++;
+             
+        }
+        if(temp!="") m[i]=temp;
+        int n=m.size();
+        for(int i=n;i>=1;i--){
+           string temp2=m[i];
+           ans=ans+temp2+" ";
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
