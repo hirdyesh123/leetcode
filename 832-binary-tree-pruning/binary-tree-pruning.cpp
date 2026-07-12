6f499ab3d9a4f7f4a3f55f8258e3598fedce24c6
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-12 18:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (77.84%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.cpp
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -10,26 +10,22 @@
  * };
  */
 class Solution {
-public: 
-    bool find(TreeNode* root){
-        if(root==NULL) return false;
-        if(root->val==1) return true;
-        bool left=find(root->left);
-        bool right=find(root->right);
-
-        return (left||right);
-    }
-       void  helper(TreeNode* root){
-        if(root==NULL) return ;
-        if(!find(root->left)) root->left=NULL;
-        if(!find(root->right)) root->right=NULL;
- 
-        helper(root->left);
-        helper(root->right);
+public:
+    int  helper(TreeNode* root){
+        if(root==NULL) return 0;
+        if(root->left==NULL && root->right==NULL){
+            if(root->val==1) return 1;
+            else return 0;
+        }
+        int left=helper(root->left);
+        int right=helper(root->right);
+        if(left==0) root->left=NULL;
+        if(right==0) root->right=NULL;
+        return left+right+root->val;
     }
     TreeNode* pruneTree(TreeNode* root) {
-        if(root->val==0 && !find(root->left) && !find(root->right)) return NULL;
         helper(root);
+        if(root->val==0 && root->left==NULL && root->right==NULL) return NULL;
         return root;
     }
 };
