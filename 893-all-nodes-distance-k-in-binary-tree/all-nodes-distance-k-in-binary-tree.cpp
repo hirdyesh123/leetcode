611ab5b9149cf5e6ee19c670ca3be19872946aa2
hirdyesh123 <hirdyeshpandey50@gmail.com>
2026-07-17 12:21:47 +0530

    Time: 4 ms (65.17%) | Memory: 15.4 MB (49.50%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -8,43 +8,44 @@
  * };
  */
 class Solution {
-public:  
-    void helper(TreeNode* root,vector<int>graph[]){
-        if(root==NULL) return ;
-        if(root->left!=NULL){
-            graph[root->val].push_back(root->left->val);
-            graph[root->left->val].push_back(root->val);
-        }
-        if(root->right!=NULL){
-            graph[root->val].push_back(root->right->val);
-            graph[root->right->val].push_back(root->val);
-        }
-        helper(root->left,graph);
-        helper(root->right,graph);
+public:
+    void helper(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&map){
+        if(root==NULL) return;
+
+        if(root->left!=NULL) map[root->left]=root;
+        if(root->right!=NULL) map[root->right]=root;
+
+        helper(root->left,map);
+        helper(root->right,map);
     }
-    
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if(root->left==NULL && root->right==NULL) return {};
-
-        vector<int>graph[501];
-        vector<int>isvisited(501,0);
-        queue<pair<int,int>>q;
-        vector<int>ans;
-        helper(root,graph);
-        q.push({target->val,0});
-        isvisited[target->val]=1;
-        while(!q.empty()){
-           auto top= q.front();
-           q.pop();
-           if(top.second==k) ans.push_back(top.first);
-           for(auto i:graph[top.first]){
-            if(isvisited[i]!=1){
-                isvisited[i]=1;
-                q.push({i,top.second+1});
+         vector<int>ans;
+         unordered_map<TreeNode*,TreeNode*>map;
+         vector<int>isvisited(501,0);
+         queue<pair<TreeNode*,int>>q;
+         q.push({target,0});
+         isvisited[target->val]=1;
+         helper(root,map);
+         while(!q.empty()){
+            auto top=q.front();
+            q.pop();
+            TreeNode* node=top.first;
+            int dist=top.second;
+            if(dist==k) ans.push_back(node->val);
+            if(node->left!=NULL && !isvisited[node->left->val]){
+                isvisited[node->left->val]=1;
+                q.push({node->left,dist+1});
             }
-           }
-        }
-
+            if(node->right!=NULL && !isvisited[node->right->val]){
+                isvisited[node->right->val]=1;
+                q.push({node->right,dist+1});
+            }
+             
+            if(map.count(node)!=0 && !isvisited[map[node]->val]){
+                isvisited[map[node]->val]=1;
+                q.push({map[node],dist+1});
+            }
+         }
         return ans;
-    }
+    } 
 };
